777d1de467362912002562b67d6bf22c6fefa342
Rocky <Rocksteady0101@users.noreply.github.com>
2022-06-07 14:55:14 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
 const container = document.querySelector('#container');
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
